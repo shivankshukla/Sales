5aca0bf8a367a7717cc0c7ba96226666756f5ba7
Shivank Shukla <Shivank Shukla@10.178.1.63>
2019-01-09 14:13:46 +0530

    Initial Checkin

---
+++ Start/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data = new SalesData();
+		data.display();
+		displayGet();
+
+	}
+	static void displayGet()
+	{
+		System.out.println("Trying");
+		
+	}
+
+}
---
+++ Start/src/SalesData.java
@@ -0,0 +1,12 @@
+
+public class SalesData {
+	
+	int data[]={1,2,3,4};
+	
+	public void display()
+	{
+		System.out.println("Todo");
+		
+	}
+
+}
